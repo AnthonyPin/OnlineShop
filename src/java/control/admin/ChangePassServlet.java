b0fa85771dbf55bd0ec2c5a7fa6bf7dacb7fbded
AnthonyPin <109681825+AnthonyPin@users.noreply.github.com>
2024-05-22 18:43:41 -0500

    Update ChangePassServlet.java

--- src/java/control/admin/ChangePassServlet.java
+++ src/java/control/admin/ChangePassServlet.java
@@ -12,9 +12,6 @@ import org.owasp.esapi.errors.IntrusionException;
 import org.owasp.esapi.errors.ValidationException;
 import persistencia.PersistenceInterface;
 
-/**
- * @author Juan Díez-Yanguas Barber
- */
 public class ChangePassServlet extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
